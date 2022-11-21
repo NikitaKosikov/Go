1422f59ab6867fcf5a85a7ff24125e9c07bf18f3
Nikita Kosikov <green.marker2@mail.ru>
2022-11-21 14:15:09 +0300

    dsagf

--- pkg/api/auth/middleware.go
+++ pkg/api/auth/middleware.go
@@ -28,7 +28,7 @@ func (m *Manager) VerifyJWTMiddleware(roles ...string) gin.HandlerFunc {
 		token, err := m.GetTokenFromString(jwtToken, claims)
 		if err != nil {
 			ctx.Redirect(http.StatusTemporaryRedirect, userURLAPI+userid+refreshTokenURI)
-			ctx.Next()
+			return
 		}
 		if err := m.ValidateToken(token, claims); err != nil {
 			ctx.Status(http.StatusForbidden)
